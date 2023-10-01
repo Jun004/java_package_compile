87585db0ba92a4daa0c9794067e018a051831932
Jun <varr285@gmail.com>
2023-10-02 01:17:07 +0900

    課題28_29のパッケージ、クラスを作成しました。

---
+++ src/object/AnimalInfo.java
@@ -0,0 +1,38 @@
+package object;
+
+public class AnimalInfo {
+	private String name;
+	private String bodyL;
+	private String speed;
+	private String scName;
+	
+	// name
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getName() {
+		return name;
+	}
+	// bodyL
+	public void setBodyL(String bodyL) {
+		this.bodyL = bodyL;
+	}
+	public String getBodyL() {
+		return bodyL;
+	}
+	// speed
+	public void setSpeed(String speed) {
+		this.speed = speed;
+	}
+	public String getSpeed() {
+		return speed;
+	}
+	// scName
+	public void setScName(String scName) {
+		this.scName = scName;
+	}
+	public String getScName() {
+		return scName;
+	}
+
+}
