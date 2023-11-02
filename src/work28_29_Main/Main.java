69e2635d04ae5c19e004ff6848928b92a9fc109b
Jun <varr285@gmail.com>
2023-11-03 01:07:19 +0900

    課題２８＿２９の進行

--- src/work28_29_Main/Main.java
+++ src/work28_29_Main/Main.java
@@ -2,29 +2,59 @@ package work28_29_Main;
 
 import java.util.Objects;
 import java.util.Scanner;
+import java.util.regex.Pattern;
+
 
 public class Main {
 	
 	public static void main(String args) {
-		String pref = "";
+		String num = "";
+		
 		int i = 0;
+		
+		String regax = "[^0-9]";
+		Pattern p = Pattern.compile(regax);
 		
 		while(i == 0) {
 			// コンソール入力
 			Scanner scan = new Scanner(System.in);
-			pref = scan.nextLine();
+			num = scan.nextLine();
+			
 			
 			// nullチェック
-			if(Objects.isNull(pref)) {
-				System.out.println("名前を入力してください");
+			if(Objects.isNull(num)) {
+				System.out.println("数字を入力してください");
 				i = 0;
-				
+			} else if(num.matches("[^0-9]")) {
+				System.out.println("数字を入力してください");
+				i = 0;
 			} else {
+				System.out.println("数字");
 				scan.close();
 				i = 1;
 			}
 		}
+		String[] nums = num.split(",");
+		int[] numArray = new int[nums.length];
 		
-		String[] prefs = pref.split(",");
+		System.out.println(numArray);
+	
+		/*String[] prefs = {"北海道:札幌市:83424"
+				, "青森県:青森市:9646"
+				, "岩手県:盛岡市:15275"
+				, "宮城県:仙台市:7282"
+				, "秋田県:秋田市:11638"
+				, "山形県:山形市:9323"
+				, "福島県:福島市:13784"
+				, "茨城県:水戸市:6097"
+				, "栃木県:宇都宮市:6408"
+				, "群馬県:前橋市:6362"
+				, "埼玉県:さいたま市:3798"};
+		
+		String[] pref;
+		
+		for(int j = 0; j < prefs.length; j++) {
+			
+		}*/
 	}
 }
--- src/work28_29_process/PrefInfo.java
+++ src/work28_29_process/PrefInfo.java
@@ -1,5 +1,31 @@
 package work28_29_process;
 
 public class PrefInfo {
+	private String name;
+	private String place;
+	private String size;
 	
+	// name
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getName() {
+		return name;
+	}
+	
+	// place
+	public void setPlace(String place) {
+		this.place = place;
+	}
+	public String getPlace() {
+		return place;
+	}
+	
+	// size
+	public void setSize(String size) {
+		this.size = size;
+	}
+	public String getSize() {
+		return size;
+	}
 }
